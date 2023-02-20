78a4b4fbaf3049ad664920b411acf72786fea245
gatolife-creater <gatolifechannel@gmail.com>
2023-02-20 22:18:30 +0900

    四角形を1個描画。

--- sketch.js
+++ sketch.js
@@ -6,4 +6,9 @@ function setup() {
 function draw() {
     // カンバスの背景を黒に設定
     background(0);
+
+    // 四角形を白に塗りつぶす
+    fill(255);
+    // (0, 0)を基準にして, 縦横200pxの四角形を描画
+    rect(0, 0, 200, 200);
 }
